ece1b24a6dc7b2836be788ed2de6cb1c25f16068
dr0id <nullvoid.nv@gmail.com>
2024-06-01 02:59:15 +0530

    url update

--- src/hooks/usesocket.ts
+++ src/hooks/usesocket.ts
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
 import { INIT_GAME, MOVE } from '../constants'
 
-const WS_URL = 'ws://localhost:5050'
+// const WS_URL = 'ws://localhost:5050'
+const WS_URL = 'wss://animeselfie.com/chess/'
 
 export const useSocket = () => {
   const [socket, setSocket] = useState<WebSocket | null>(null)
